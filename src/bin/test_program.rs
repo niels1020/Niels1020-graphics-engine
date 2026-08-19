84f66ebba1e1de469fd5d164243db1977bd8403f
Mathijs De Smet <mathijsniels10@outlook.com>
2026-08-19 17:49:42 +0200

    cleanup

--- src/bin/test_program.rs
+++ src/bin/test_program.rs
@@ -5,7 +5,7 @@ use wgpu_game_engine::{
         game_window::{GameInfo, InputHandler},
     },
     render::{
-        render_2d::{self, camera::Camera2D, layer::RenderLayer2D, render_objects::text::Text},
+        render_2d::{camera::Camera2D, layer::RenderLayer2D, render_objects::text::Text},
         render_layers::layer_as_type_mut,
     },
     start_engine,
@@ -81,12 +81,12 @@ impl InputHandler for Input {
 
     fn start(&mut self, _commands: &mut Commands, game_info: &mut GameInfo) {
         let mut layer1 = RenderLayer2D::new(
-            include_wgsl!("../../assets/2d.wgsl"),
+            include_wgsl!("../../assets/test/2d.wgsl"),
             "Test 2D".to_string(),
             Camera2D::new([800.0, 600.0]),
         );
-        layer1.add_child(render_2d::render_objects::test::VerticesTest::new());
-        layer1.add_child(render_2d::render_objects::test::TextureTest::new());
+        layer1.add_child(VerticesTest::new());
+        layer1.add_child(TextureTest::new());
         layer1.add_child(Text::new(
             "0xProto Nerd Font".to_string(),
             "Hello, world! ".to_string(),
@@ -105,5 +105,101 @@ impl Input {
         Self {
             resolution: [1.0; 2],
         }
+    }
+}
+
+use wgpu_game_engine::{
+    common::Vertex,
+    render::render_2d::layer::{RenderLayer2DGlobal, RenderObject2D},
+};
+
+pub struct VerticesTest {
+    has_updated: bool,
+}
+
+impl VerticesTest {
+    pub fn new() -> Box<Self> {
+        Box::new(Self { has_updated: false })
+    }
+}
+
+impl RenderObject2D for VerticesTest {
+    fn have_vertices_changed(&mut self) -> bool {
+        !self.has_updated
+    }
+
+    fn get_vertices(&mut self, _: &mut RenderLayer2DGlobal) -> Vec<Vertex> {
+        self.has_updated = true;
+        vec![
+            Vertex::new(400.0, 0.0, 0.01, 0.0, 0.0, 0),
+            Vertex::new(400.0, 300.0, 0.01, 0.0, 0.0, 0),
+            Vertex::new(0.0, 300.0, 0.01, 0.0, 0.0, 0),
+            Vertex::new(-400.0, 0.0, 0.01, 0.0, 0.0, 0),
+            Vertex::new(-400.0, -300.0, 0.01, 0.0, 0.0, 0),
+            Vertex::new(0.0, -300.0, 0.01, 0.0, 0.0, 0),
+        ]
+    }
+
+    fn get_name(&self) -> String {
+        "VERTICES TEST".to_string()
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+}
+
+pub struct TextureTest {
+    has_updated: bool,
+    image_added: bool,
+}
+
+impl TextureTest {
+    pub fn new() -> Box<Self> {
+        Box::new(Self {
+            has_updated: false,
+            image_added: false,
+        })
+    }
+}
+
+impl RenderObject2D for TextureTest {
+    fn have_vertices_changed(&mut self) -> bool {
+        !self.has_updated
+    }
+
+    fn get_vertices(&mut self, global: &mut RenderLayer2DGlobal) -> Vec<Vertex> {
+        if !self.image_added {
+            self.image_added = true;
+            global.atlas_texture.add_image(
+                image::load_from_memory(include_bytes!("../../assets/test/profiel.png")).unwrap(),
+                "profiel".to_string(),
+            );
+        }
+
+        self.has_updated = true;
+
+        let rect = global
+            .atlas_texture
+            .get_relative_texture_rect("profiel".to_string())
+            .unwrap();
+        let (top_left, top_right, bottom_left, bottom_right) = rect.bounds();
+
+        vec![
+            Vertex::new(-50.0, -50.0, 0.01, bottom_left.0, bottom_left.1, 1),
+            Vertex::new(50.0, -50.0, 0.01, bottom_right.0, bottom_right.1, 1),
+            Vertex::new(-50.0, 50.0, 0.01, top_left.0, top_left.1, 1),
+            Vertex::new(50.0, -50.0, 0.01, bottom_right.0, bottom_right.1, 1),
+            Vertex::new(50.0, 50.0, 0.01, top_right.0, top_right.1, 1),
+            Vertex::new(-50.0, 50.0, 0.01, top_left.0, top_left.1, 1),
+        ]
+    }
+
+    fn get_name(&self) -> String {
+        "TEXTURE TEST".to_string()
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
     }
 }
