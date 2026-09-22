6b8b2e03cdcbfbf09d37b9248849b4ea87784f9f
niels1020 <mathijsniels10@gmail.com>
2026-09-22 19:47:08 +0200

    fixed not all events being pushed to the queue (replaced .try_lock with .lock) and added panics for if an even is skipt

--- src/logic/game_window.rs
+++ src/logic/game_window.rs
@@ -112,14 +112,18 @@ impl GameWindow {
                 }
             }
         }
-        if let Ok(mut shared) = self.shared_logic_info.as_ref().unwrap().try_lock() {
+        if let Ok(mut shared) = self.shared_logic_info.as_ref().unwrap().lock() {
             shared.window_events.push_back((event, window_id));
+        } else {
+            panic!("could not lock window event queue")
         }
     }
 
     pub fn device_event(&mut self, event: DeviceEvent, device_id: DeviceId) {
-        if let Ok(mut shared) = self.shared_logic_info.as_ref().unwrap().try_lock() {
+        if let Ok(mut shared) = self.shared_logic_info.as_ref().unwrap().lock() {
             shared.device_events.push_back((event, device_id));
+        } else {
+            panic!("could not lock device event queue")
         }
     }
 }
